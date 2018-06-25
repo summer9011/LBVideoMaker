6f48931791eefd40d53bcabdc583bbc859f6b181
胡萝卜_小波波 <451973176@qq.com>
2018-06-25 22:49:05 +0800

    add screenplay

---
+++ Example/LBVideoMaker/LBScreenplay.h
@@ -0,0 +1,13 @@
+//
+//  LBScreenplay.h
+//  LBVideoMaker_Example
+//
+//  Created by 赵立波 on 2018/6/25.
+//  Copyright © 2018年 胡萝卜_小波波. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LBScreenplay : NSObject
+
+@end
