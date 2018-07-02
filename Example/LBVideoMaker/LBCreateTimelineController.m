4932f61a68c91ee876b096eb1d3ef666d246a1be
Leebo <zhao_li_bo@163.com>
2018-07-02 23:46:18 +0800

    leebo
    add actionview.

--- Example/LBVideoMaker/LBCreateTimelineController.m
+++ Example/LBVideoMaker/LBCreateTimelineController.m
@@ -11,13 +11,11 @@
 
 #import <ReactiveObjC/ReactiveObjC.h>
 
-@interface LBCreateTimelineController () <UIScrollViewDelegate>
+@interface LBCreateTimelineController ()
 
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *doneBarBtnItem;
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
-@property (nonatomic, assign) BOOL isScrolling;
 
 @end
 
@@ -27,32 +25,12 @@
     [super viewDidLoad];
     
     self.scrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width + 500, [UIScreen mainScreen].bounds.size.height + 500);
-    
-    [RACObserve(self, isScrolling) subscribeNext:^(id  _Nullable x) {
-        NSLog(@"isScrolling %d", [x boolValue]);
-    }];
 }
 
 #pragma mark - Actions
 
 - (IBAction)doDoneAction:(id)sender {
     [self.navigationController dismissViewControllerAnimated:YES completion:nil];
-}
-
-#pragma mark - UIScrollViewDelegate
-
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
-    self.isScrolling = YES;
-}
-
-- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
-    if (!decelerate) {
-        self.isScrolling = NO;
-    }
-}
-
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    self.isScrolling = NO;
 }
 
 #pragma mark - Segue
