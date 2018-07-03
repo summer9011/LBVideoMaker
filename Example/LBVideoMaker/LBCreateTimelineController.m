edafe05b8cc68f19923df2f5f60b637f5f66674f
leebo <zhao_li_bo@163.com>
2018-07-03 16:04:34 +0800

    add pin view.

--- Example/LBVideoMaker/LBCreateTimelineController.m
+++ Example/LBVideoMaker/LBCreateTimelineController.m
@@ -7,6 +7,7 @@
 //
 
 #import "LBCreateTimelineController.h"
+#import "LBTimelineView.h"
 #import "LBResourceListController.h"
 
 #import <ReactiveObjC/ReactiveObjC.h>
@@ -17,6 +18,9 @@
 
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
 
+@property (nonatomic, strong) LBPinTimelineView *beginPin;
+@property (nonatomic, strong) LBPinTimelineView *endPin;
+
 @end
 
 @implementation LBCreateTimelineController
@@ -25,6 +29,8 @@
     [super viewDidLoad];
     
     self.scrollView.contentSize = CGSizeMake([UIScreen mainScreen].bounds.size.width + 500, [UIScreen mainScreen].bounds.size.height + 500);
+    
+    [self.scrollView addSubview:self.beginPin];
 }
 
 #pragma mark - Actions
@@ -33,12 +39,58 @@
     [self.navigationController dismissViewControllerAnimated:YES completion:nil];
 }
 
+#pragma mark - Gesture
+
+- (void)panOnView:(UIPanGestureRecognizer *)pan {
+    LBTimelineView *view = (LBTimelineView *)pan.view;
+    
+    CGPoint offset = [pan translationInView:view];
+    CGPoint point = view.center;
+    point.x += offset.x;
+    view.center = point;
+    [pan setTranslation:CGPointZero inView:view];
+    
+    if (pan.state == UIGestureRecognizerStateEnded) {
+        NSUInteger p = 10;
+        
+        CGFloat x = floorf(point.x/p);
+        CGFloat leave = point.x - x*p;
+        if (leave < p*0.5) {
+            point.x = x*p;
+        } else {
+            point.x = (x+1)*p;
+        }
+        
+        [UIView animateWithDuration:0.1 animations:^{
+            view.center = point;
+        }];
+    }
+}
+
 #pragma mark - Segue
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     UINavigationController *naviVC = (UINavigationController *)segue.destinationViewController;
     LBResourceListController *resouceListVC = (LBResourceListController *)naviVC.viewControllers.firstObject;
     resouceListVC.obj = self.obj;
+}
+
+#pragma mark - Getting
+
+- (LBPinTimelineView *)beginPin {
+    if (_beginPin == nil) {
+        _beginPin = [LBPinTimelineView new];
+        UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(panOnView:)];
+        [_beginPin addGestureRecognizer:pan];
+    }
+    return _beginPin;
+}
+
+- (LBPinTimelineView *)endPin {
+    if (_endPin == nil) {
+        _endPin = [LBPinTimelineView new];
+    }
+    return _endPin;
 }
 
 @end
---
+++ Example/LBVideoMaker/LBTimelineView.h
@@ -0,0 +1,17 @@
+//
+//  LBTimelineView.h
+//  LBVideoMaker_Example
+//
+//  Created by 赵立波 on 2018/7/3.
+//  Copyright © 2018年 胡萝卜_小波波. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBTimelineView : UIView <UIDynamicItem>
+
+@end
+
+@interface LBPinTimelineView : LBTimelineView
+
+@end
